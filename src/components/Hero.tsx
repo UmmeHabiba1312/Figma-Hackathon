cbd2bb1c14dd4e24c45d35d2edcf5dde45fe8fac
UmmeHabiba1312 <gm3088685@gmail.com>
2024-12-12 13:03:38 +0500

    first comment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,42 @@
-import React from 'react'
-import { IoMdArrowForward } from 'react-icons/io'
+import React from 'react';
+import { IoMdArrowForward } from 'react-icons/io';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <main className='h-auto lg:h-[850px] w-full mx-auto max-w-screen-2xl bg-[#f0f2f3]  lg:flex  items-center'>
+    <main className='h-auto lg:h-[850px] w-full mx-auto max-w-screen-2xl bg-[#f0f2f3] flex flex-col lg:flex-row items-center px-4 sm:px-10 lg:px-0'>
 
-        {/* left */}
-        <div className='h-[337px] flex-1 flex flex-col justify-center md:pl-10 pl-5'>
-            <h4 className={`${inter.className} text-[14px] leading-[14px] text-[#272343]`}>Welcome to chairy</h4>
-            <h2 className={`${inter.className} sm:text-[60px] text-[45px] leading-[66px] text-[#272343] mt-[20px]`}>Best Furniture
-Collection for your
-interior.</h2>
-<button className='rounded-[8px] h-[52px] w-[171px] text-[#FFFFFF] mt-[30px] px-[24px] py-[14px] gap-[20px] flex justify-center items-center bg-[#029FAE] text-[16px] leading-[17px]'>Shop Now <IoMdArrowForward /></button>
-        </div>
+      {/* Left Section */}
+      <div className='flex-1 flex flex-col justify-center text-center lg:text-left'>
+        <h4 className={`${inter.className} text-[14px] leading-[14px] text-[#272343]`}>
+          Welcome to Chairy
+        </h4>
+        <h2
+          className={`${inter.className} text-[32px] sm:text-[45px] lg:text-[60px] leading-[36px] sm:leading-[50px] lg:leading-[66px] text-[#272343] mt-4`}
+        >
+          Best Furniture Collection for your interior.
+        </h2>
+        <button
+          className='rounded-[8px] h-[52px] w-full sm:w-[171px] text-[#FFFFFF] mt-6 sm:mt-[30px] px-4 sm:px-[24px] py-2 flex justify-center items-center bg-[#029FAE] text-[16px] leading-[17px]'
+        >
+          Shop Now <IoMdArrowForward className='ml-2' />
+        </button>
+      </div>
 
-        {/* right */}
-        <div className='h-auto w-auto flex-1 flex justify-center items-center'>
-            <Image
-            className='sm:h-[584px] h-[484px] sm:w-[404px] w-full object-cover '
-            height={100}
-            width={500}
-            src='/hero.png'
-            alt='hero'/>
-        </div>
+      {/* Right Section */}
+      <div className='flex-1 h-auto w-auto flex justify-center items-center mt-6 lg:mt-0'>
+        <Image
+          className='h-auto w-full sm:w-[404px] md:w-[554px] lg:h-[804px] lg:w-[650px] '
+          height={100}
+          width={500}
+          src='/hero.png'
+          alt='hero'
+        />
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
