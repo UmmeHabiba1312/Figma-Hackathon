cbc74cf75c3c62ee07d783490bebbd0798684703
UmmeHabiba1312 <gm3088685@gmail.com>
2024-12-22 17:34:04 +0500

    first comment

--- src/components/AboveFot.tsx
+++ src/components/AboveFot.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 const BeforFoter = () => {
   return (
     <main className="mt-[60px] w-full">
-      <div className="flex justify-around w-full h-auto items-center  ">
+      <div className="flex justify-center w-full h-auto items-center  ">
         <div className="max-w-screen-xl w-full h-auto sm:h-[270px] items-center bg-[#F4F4F4] grid grid-cols-1 gap-8 sm:gap-0 sm:grid-cols-2 lg:grid-cols-3 px-4 lg:px-16 py-8">
           
             {/* High Quality */}
-            <div className="flex items-center gap-4  justify-center">
+            <div className="flex items-center gap-4  justify-start  lg:justify-center">
               <Image
                 className="h-[60px] w-[60px]"
                 height={60}
@@ -23,7 +23,7 @@ const BeforFoter = () => {
             </div>
 
             {/* Warranty Protection */}
-            <div className="flex items-center gap-4  justify-center">
+            <div className="flex items-center gap-4  justify-start  lg:justify-center">
               <Image
                 className="h-[60px] w-[60px]"
                 height={60}
@@ -38,7 +38,7 @@ const BeforFoter = () => {
             </div>
 
             {/* 24 / 7 Support */}
-            <div className="flex items-center gap-4  justify-center">
+            <div className="flex items-center gap-4 justify-start  lg:justify-center">
               <Image
                 className="h-[60px] w-[60px]"
                 height={60}
